b0c99ba4874f50d23ee83e24094eb5e66eb43b1e
sabarjp <jsh.a.powers@gmail.com>
2015-07-23 20:59:17 -0500

    migrated part of meteor build to rails, added react, added typescript

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,18 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Tonycrafter"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Tonycrafter"
   end
 
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | Tonycrafter"
+  end
 end
